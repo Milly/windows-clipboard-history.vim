8513ae5385944a2a3f3fc0be9140bc4e8200546f
Milly <milly.ca@gmail.com>
2022-11-22 17:56:09 +0900

    fix: refactor: fixed unprintable highlights and kind
    - Fixed to detection unprintable characters.
    - Show kind like 'l', 'c' insetad 'V', 'v'.
    - Refactored methods.

--- denops/@ddc-sources/windows-clipboard-history.ts
+++ denops/@ddc-sources/windows-clipboard-history.ts
@@ -29,12 +29,14 @@ type Params = {
    * If 0 is specified, be unlimited.
    */
   maxAbbrWidth: number;
-  /** Highlight group name for control-chars. (default: "SpecialKey")
+  /** Highlight group name for unprintable chars. (default: "SpecialKey")
    *
    * If empty, highlight is disabled.
    */
   ctrlCharHlGroup: string;
 };
+
+type OperatorWise = "c" | "l" | "b" | "";
 
 type UserData = {
   text: string;
@@ -45,13 +47,21 @@ type UserData = {
 type Item = SetRequired<DdcItem<UserData>, "abbr" | "user_data">;
 
 const ID = "ddc/source/windows-clipboard-history";
-// deno-lint-ignore no-control-regex
-const reControlChar = /[\x01-\x1f]/g;
-const ctrlChars = 2; // "^@".length
-const ctrlBytes = 2; // strlen("^@")
+
+// deno-fmt-ignore
+const UNPRINTABLE_CHARS = [
+  0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+  0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+  0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+  0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
+] as const;
+const UNPRINTABLE_CHAR_LENGTH = 2; // "^@".length
+const UNPRINTABLE_BYTE_LENGTH = 2; // strlen("^@")
 
 export class Source extends BaseSource<Params, UserData> {
   #clipboardHistory: ClipboardHistory | undefined;
+  // deno-lint-ignore no-control-regex
+  #reUnprintableChar = /[\x00-\x1f]/g;
 
   override params(): Params {
     return {
@@ -63,8 +73,9 @@ export class Source extends BaseSource<Params, UserData> {
   }
 
   override async onInit(args: OnInitArguments<Params>): Promise<void> {
-    const { sourceParams: { pwsh, cacheExpires } } = args;
+    const { denops, sourceParams: { pwsh, cacheExpires } } = args;
     this.#clipboardHistory = new ClipboardHistory({ pwsh, cacheExpires });
+    this.#reUnprintableChar = await this.#getUnprintableRegExp(denops);
     await this.#clipboardHistory.waitInitialized();
   }
 
@@ -82,19 +93,17 @@ export class Source extends BaseSource<Params, UserData> {
     } = args;
 
     const [abbrWidth, recentHistory] = await Promise.all([
-      globalOptions.get(denops, "columns", 9999).then((vimColumns) =>
-        maxAbbrWidth > 0 ? Math.min(maxAbbrWidth, vimColumns) : vimColumns
-      ),
+      this.#getAbbrWidth(denops, maxAbbrWidth),
       this.#clipboardHistory.getHistory().then((history) =>
         history.slice(0, Math.max(1, maxItems))
       ),
     ]);
 
-    const items = generateItems(recentHistory, nextInput);
-    await truncateItemsAbbr(denops, items, abbrWidth);
+    const items = this.#generateItems(recentHistory, nextInput);
+    await this.#applyAbbrWidth(denops, items, abbrWidth);
 
     if (ctrlCharHlGroup) {
-      await addItemsCtrlCharHighlights(denops, items, ctrlCharHlGroup);
+      await this.#applyHighlights(denops, items, ctrlCharHlGroup);
     }
 
     return items;
@@ -110,15 +119,15 @@ export class Source extends BaseSource<Params, UserData> {
     } = args;
     const line = input + nextInput;
     const isConfirmed = line.endsWith(word + suffix);
-    const hasCtrlChar = reControlChar.test(text);
+    const hasUnprintable = this.#reUnprintableChar.test(text);
 
-    if (isConfirmed && hasCtrlChar) {
+    if (isConfirmed && hasUnprintable) {
       const prefix = line.slice(0, line.length - word.length - suffix.length);
       const newText = prefix + text + suffix;
       const cursorCol = newText.length - nextInput.length;
       const prevText = newText.slice(0, cursorCol);
       const nextText = newText.slice(cursorCol);
-      const lines = (prevText + nextText).split("\n");
+      const lines = textToRegContents(prevText + nextText);
 
       const prevLines = prevText.split("\n");
       const newLnum = lineNr + prevLines.length - 1;
@@ -133,121 +142,167 @@ export class Source extends BaseSource<Params, UserData> {
       );
     }
   }
-}
 
-function generateItems(
-  history: ClipboardHistoryItem[],
-  suffix: string,
-): Item[] {
-  const now = Date.now();
-  return history.map((hist): Item => {
-    const text = hist.Text.replaceAll("\r\n", "\n");
-    const word = text.replaceAll(reControlChar, "?");
-    const abbr = text.replaceAll("\n", "^@")
-      .replaceAll(
-        reControlChar,
-        (c) => "^" + String.fromCharCode(c.charCodeAt(0) + 0x40),
+  async #getUnprintableRegExp(denops: Denops): Promise<RegExp> {
+    // generate RegExp e.g.: /[\x00-\x1f\x7f-\x9f]/g
+    const unprintable = new Set([
+      ...UNPRINTABLE_CHARS,
+      ...(await getUnprintableChars(denops)),
+    ]);
+    const lastGuard = 0x100;
+    unprintable.delete(lastGuard);
+    const xhh = (n: number) => "\\x" + `0${n.toString(16)}`.slice(-2);
+    const range: string[] = [];
+    for (let start = -1, code = 0; code <= lastGuard; ++code) {
+      if (start < 0 && unprintable.has(code)) {
+        start = code;
+      } else if (start >= 0 && !unprintable.has(code)) {
+        const end = code - 1;
+        range.push(start === end ? xhh(start) : xhh(start) + "-" + xhh(end));
+        start = -1;
+      }
+    }
+    return new RegExp(`[${range.join("")}]`, "g");
+  }
+
+  async #getAbbrWidth(denops: Denops, maxAbbrWidth: number): Promise<number> {
+    const vimColumns = await globalOptions.get(denops, "columns", 9999);
+    return maxAbbrWidth > 0 ? Math.min(maxAbbrWidth, vimColumns) : vimColumns;
+  }
+
+  #generateItems(history: ClipboardHistoryItem[], suffix: string): Item[] {
+    const now = Date.now();
+    return history.map((hist): Item => {
+      const text = hist.Text.replaceAll("\r\n", "\n");
+      const wise: OperatorWise = text.endsWith("\n") ? "l" : "c";
+      const word = text.replaceAll(this.#reUnprintableChar, "?");
+      const abbr = text.replaceAll(
+        this.#reUnprintableChar,
+        unpritableCharConverter,
       );
-    const regType = text.endsWith("\n") ? "V" : "v";
-    return {
-      word,
-      abbr,
-      info: text,
-      kind: regType,
-      menu: toDuration(now - hist.Time),
-      user_data: {
-        text,
+      const duration = toDuration(now - hist.Time);
+      return {
         word,
-        suffix,
-      },
-    };
-  });
-}
+        abbr,
+        info: text,
+        kind: wise,
+        menu: duration,
+        user_data: {
+          text,
+          word,
+          suffix,
+        },
+      };
+    });
+  }
 
-async function truncateItemsAbbr(
-  denops: Denops,
-  items: Item[],
-  maxWidth: number,
-): Promise<void> {
-  const truncated = await bulk.printf(
-    denops,
-    `%.${maxWidth}S`,
-    items.map(({ abbr }) => abbr),
-  );
-  items.forEach((item, i) => {
-    item.abbr = truncated[i];
-  });
-}
+  async #applyAbbrWidth(
+    denops: Denops,
+    items: Item[],
+    abbrWidth: number,
+  ): Promise<void> {
+    const truncated = await bulk.printf(
+      denops,
+      `%.${abbrWidth}S`,
+      items.map(({ abbr }) => abbr),
+    );
+    items.forEach((item, i) => {
+      item.abbr = truncated[i];
+    });
+  }
 
-async function addItemsCtrlCharHighlights(
-  denops: Denops,
-  items: Item[],
-  hlGroup: string,
-): Promise<void> {
-  const itemSlices = items.map((item) => ({
-    item,
-    slices: item.user_data.text.slice(0, item.abbr.length)
-      .split(reControlChar).slice(0, -1),
-  }));
-  const sliceBytes = await bulk.strlen(
-    denops,
-    itemSlices.map(({ slices }) => slices).flat(),
-  );
-  let sliceBytesIndex = 0;
-  const itemSliceBytes = itemSlices.map(({ item, slices }) => ({
-    item,
-    slices: slices.map((slice) => ({
-      chars: slice.length,
-      bytes: sliceBytes[sliceBytesIndex++],
-    })),
-  }));
+  async #applyHighlights(
+    denops: Denops,
+    items: Item[],
+    hlGroup: string,
+  ): Promise<void> {
+    const itemSlices = items.map((item) => {
+      const { abbr, user_data } = item;
+      const text = user_data.text.slice(0, abbr.length);
+      const slices = text.split(this.#reUnprintableChar).slice(0, -1);
+      return { item, slices };
+    });
+    const sliceBytes = await bulk.strlen(
+      denops,
+      itemSlices.map(({ slices }) => slices).flat(),
+    );
+    let sliceBytesIndex = 0;
+    const itemSliceBytes = itemSlices.map(({ item, slices }) => ({
+      item,
+      slices: slices.map((slice) => ({
+        chars: slice.length,
+        bytes: sliceBytes[sliceBytesIndex++],
+      })),
+    }));
 
-  for (const { item, slices } of itemSliceBytes) {
-    if (slices.length > 0) {
-      item.highlights = generateCtrlCharHighlights(item.abbr, slices, hlGroup);
+    for (const { item, slices } of itemSliceBytes) {
+      if (slices.length > 0) {
+        item.highlights = this.#generateHighlights(item.abbr, slices, hlGroup);
+      }
     }
   }
-}
 
-function generateCtrlCharHighlights(
-  abbr: string,
-  abbrSlices: { chars: number; bytes: number }[],
-  hlGroup: string,
-): PumHighlight[] {
-  const highlights: PumHighlight[] = [];
-  let lastHighlight: PumHighlight | undefined;
-  let len = 0; // [chars]
-  let col = 0; // [bytes]
+  #generateHighlights(
+    abbr: string,
+    abbrSlices: { chars: number; bytes: number }[],
+    hlGroup: string,
+  ): PumHighlight[] {
+    const highlights: PumHighlight[] = [];
+    let lastHighlight: PumHighlight | undefined;
+    let len = 0; // [chars]
+    let col = 0; // [bytes]
 
-  for (const { chars, bytes } of abbrSlices) {
-    if (bytes === 0 && lastHighlight) {
-      // increase width
-      lastHighlight.width += ctrlBytes;
-    } else {
-      len += chars;
-      col += bytes;
-      if (len >= abbr.length) {
-        break;
+    for (const { chars, bytes } of abbrSlices) {
+      if (bytes === 0 && lastHighlight) {
+        // increase width
+        lastHighlight.width += UNPRINTABLE_BYTE_LENGTH;
+      } else {
+        len += chars;
+        col += bytes;
+        if (len >= abbr.length) {
+          break;
+        }
+
+        // add new highlight
+        lastHighlight = {
+          name: `${ID}/unprintable`,
+          type: "abbr",
+          hl_group: hlGroup,
+          col,
+          width: UNPRINTABLE_BYTE_LENGTH,
+        };
+        highlights.push(lastHighlight);
       }
 
-      // add new highlight
-      lastHighlight = {
-        name: `${ID}/ctrlchar`,
-        type: "abbr",
-        hl_group: hlGroup,
-        col,
-        width: ctrlBytes,
-      };
-      highlights.push(lastHighlight);
+      len += UNPRINTABLE_CHAR_LENGTH;
+      col += UNPRINTABLE_BYTE_LENGTH;
+      if (len >= abbr.length) {
+        lastHighlight.width -= len - abbr.length;
+        break;
+      }
     }
 
-    len += ctrlChars;
-    col += ctrlBytes;
-    if (len >= abbr.length) {
-      lastHighlight.width -= len - abbr.length;
-      break;
-    }
+    return highlights;
   }
+}
 
-  return highlights;
+function textToRegContents(text: string): string[] {
+  return text.split("\n").map((s) => s.replaceAll("\x00", "\n"));
+}
+
+function unpritableCharConverter(c: string): string {
+  const code = c.charCodeAt(0);
+  if (code <= 0x1f) return "^" + String.fromCharCode(code + 0x40);
+  if (code === 0x7f) return "^?";
+  if (code <= 0x9f) return "~" + String.fromCharCode(code - 0x40);
+  if (code <= 0xfe) return "|" + String.fromCharCode(code - 0x80);
+  return "~?";
+}
+
+function getUnprintableChars(
+  denops: Denops,
+): Promise<number[]> {
+  return denops.eval(
+    "range(0x100)->filter({ _, n -> nr2char(n) !~# '\\p' })",
+  ) as Promise<number[]>;
 }
